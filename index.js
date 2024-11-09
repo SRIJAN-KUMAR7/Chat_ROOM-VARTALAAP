d6cdc76a82ebb1ecf9ea13e5e0f23fffebf29106
SRIJAN KUMAR <158801253+SRIJAN-KUMAR7@users.noreply.github.com>
2024-11-09 18:28:44 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,17 +1,35 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server); // Create socket.io server
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// When a user connects
+io.on("connection", (socket) => {
+  console.log("A user connected");
+
+  // Notify others when a user joins
+  socket.on("newUser", (username) => {
+    socket.username = username; // Store the username in socket object
+    socket.broadcast.emit("user-connected", username); // Emit to everyone except the sender
+  });
+
+  // Handle incoming messages
+  socket.on("sendMessage", (message) => {
+    io.emit("message", message); // Broadcast message to all users
+  });
+
+  // Notify when a user disconnects
+  socket.on("disconnect", () => {
+    if (socket.username) {
+      socket.broadcast.emit("user-disconnected", socket.username);
+    }
+  });
+});
+
+// Start the server
+server.listen(4000, () => {
+  console.log("Server is running on http://localhost:4000");
+});
